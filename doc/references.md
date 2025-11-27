3ba665d651bca921cd3c7de1d1a01ebd96458f47
dmytro <dziumanenkod@gmail.com>
2025-11-27 16:27:12 +0000

    feat: measure time helper

---
+++ doc/references.md
@@ -0,0 +1,5 @@
+# References
+
+---
+### Measuring execution time of a function in C++
+https://stackoverflow.com/questions/22387586/measuring-execution-time-of-a-function-in-c
--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
+#include <chrono>
+
+
+using namespace std;
+
+
+template<typename Func, typename... Args>
+void measureTime(Func &&func) {
+    auto start = chrono::high_resolution_clock::now();
+    func();
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double, milli> ms = end - start;
+
+    cout << "Time: " << ms.count() << " ms\n";
+}
+
+
+void test1() {
+    for (volatile int i = 0; i < 100000000; ++i);
+}
 
 int main() {
-    std::cout << "Test"<< std::endl;
+    measureTime(test1);
+    return 0;
 }
