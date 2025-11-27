1f8b5bcc21b817f750646bb5e1cd17e4161fd71c
dmytro <dziumanenkod@gmail.com>
2025-11-27 20:06:55 +0000

    feat: add analysis.md

---
+++ doc/analysis.md
@@ -0,0 +1,44 @@
+# Big-O claims and measured results
+
+---
+
+### Dijkstra's algorithm and travelling salesman problem
+
+#### Big-O
+
+n - number of cities
+m - number of routes
+
+    TIME: O(m log n)
+    SPACE: O(n + m)
+
+#### Measures
+
+![dijkstra time](../assets/dijkstra_time.png)
+
+### Candy
+
+#### Big-O
+
+n - number of children
+
+    TIME: O(n)
+    SPACE: O(1)
+
+[//]: # (#### Measures)
+
+[//]: # (![Candy time]&#40;../assets/candy_time.png;)
+
+### Find Celebrity
+
+#### Big-O
+
+n - number of children
+
+    TIME: O(n)
+    SPACE: O(1)
+
+[//]: # (#### Measures)
+
+[//]: # (![Candy time]&#40;../assets/candy_time.png;)
+
--- main.cpp
+++ main.cpp
@@ -9,17 +9,18 @@
 using namespace std;
 
 
-std::vector<std::vector<bool>> FindCelebrity::knowsMatrix;
+std::vector<std::vector<bool> > FindCelebrity::knowsMatrix;
 std::vector<int> ratings;
 
 template<typename Func, typename... Args>
-void measureTime(Func &&func) {
+double measureTime(Func &&func, Args &&... args) {
     auto start = chrono::high_resolution_clock::now();
-    func();
+    func(forward<Args>(args)...);
+    // https://medium.com/cpluspluschronicles/a-developers-guide-to-std-forward-and-fold-expressions-in-c-2cf1fa8a53e0
     auto end = chrono::high_resolution_clock::now();
     chrono::duration<double, milli> ms = end - start;
 
-    cout << "Time: " << ms.count() << " ms\n";
+    return ms.count();
 }
 
 
@@ -27,14 +28,37 @@ void test1() {
     for (volatile int i = 0; i < 100000000; ++i);
 }
 
+template<typename Func, typename... Args>
+void benchmark(const int loops, Func &&func, Args &&... args) {
+    double total = 0;
+    for (int i = 0; i < loops; i++) {
+        total += measureTime(func, forward<Args>(args)...);
+    }
+
+    cout << endl;
+    cout << "Loops: " << loops << endl;
+    cout << "Total time: " << total << endl;
+    cout << "Average loop time: " << total / loops << endl;
+    cout << endl;
+}
+
 int main() {
-    measureTime(dijkstra);
+    ratings = {1, 2, 3, 2, 1};
+    benchmark(100, dijkstra);
+    // benchmark(100, candy(ratings));
+
+    cout << "Travelling salesman problem" << endl;
+    map<string, int> shortestDistance = dijkstra();
+    for (const auto &pair: shortestDistance) {
+        cout << pair.first << " - " << pair.second << endl;
+    }
 
     FindCelebrity::knowsMatrix = {
         {false, true, true},
         {false, false, true},
         {false, false, false}
     };
+    cout << "Find Celebrity problem (2)" << endl;
     cout << FindCelebrity::findCelebrity(3) << endl;
 
     FindCelebrity::knowsMatrix = {
@@ -42,14 +66,18 @@ int main() {
         {false, false, false},
         {true, true, false}
     };
+    cout << "Find Celebrity problem (1)" << endl;
     cout << FindCelebrity::findCelebrity(3) << endl;
 
     FindCelebrity::knowsMatrix = {
         {false}
     };
+    cout << "Find Celebrity problem (0)" << endl;
     cout << FindCelebrity::findCelebrity(1) << endl;
-    ratings = {1,2,3,2,1};
+
+
+    cout << "Candies problem" << endl;
     int candies = Candy::candy(ratings);
-    cout <<candies << endl;
+    cout << candies << endl;
     return 0;
 }
